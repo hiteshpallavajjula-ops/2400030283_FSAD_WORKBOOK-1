9ebb8826cc12065d866bf8ba5e9f6b971dea7e67
hitesh pvm <2400030283@kluniversity.in>
2026-04-10 07:29:11 +0530

    Added all projects

--- Student_manager/src/main/java/com/student/studentmanager/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.student.studentmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.student.studentmanager.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Integer>{
-
-}
